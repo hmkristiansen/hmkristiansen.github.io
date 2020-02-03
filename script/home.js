99d6af91be395c24e14878deba91b6016cbf2d9f
Hinken <henkrist@users.noreply.github.com>
2020-02-03 20:17:07 +0000

    revamped the layout for the home page

--- script/home.js
+++ script/home.js
@@ -15,6 +15,48 @@ function initilize() {
 	addPortDivs();
 }
 
+function addPortDivs(){
+	
+	var portSection = document.querySelector('.portfolio'); 
+
+	for(var i=0; i < projects.length; i++){
+		var div = document.createElement('div');
+		div.className = 'portItem right';
+		
+		if(i%2 == 0){
+			div.className = 'portItem';
+		}
+		portSection.appendChild(div);
+	}
+	populatePort();	
+}
+
+function populatePort(){
+
+	var portElements = document.querySelectorAll('.portItem');
+	
+	for(var i=0; i < portElements.length; i++){
+	
+		var atag = document.createElement('a');
+		
+		var img = document.createElement('img');
+		img.src = "img/" + projects[i][0] + "/" + projects[i][0] + ".jpg"; 
+		
+		var h1 = document.createElement('h1');
+		h1.innerText = projects[i][2];
+		
+		var h3 = document.createElement('h3');
+		h3.innerText = projects[i][1];
+		
+		atag.appendChild(img);
+		atag.appendChild(h1);
+		atag.appendChild(h3);
+		
+		portElements[i].appendChild(atag);
+	}
+}
+
+/*
 function addPortDivs() {
 	var currentRow;
 
@@ -37,8 +79,9 @@ function addPortDivs() {
 	
 	populateColumns();
 	
-}
+}*/
 
+/*
 function populateColumns(){
 
 	var columnElements = document.querySelectorAll('.column');
@@ -61,16 +104,9 @@ function populateColumns(){
 		atag.appendChild(h3);
 		
 		columnElements[i].appendChild(atag);
-		
-		/*
-		columnElements[i].appendChild(img);
-		columnElements[i].appendChild(h1);
-		columnElements[i].appendChild(h3);
-		*/
-		
 	}
 }
-
+*/
 
 
 
