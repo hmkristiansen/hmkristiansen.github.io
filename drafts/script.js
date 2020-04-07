20f8b8dcefe8e86a0377d60e64e954b529bf1c68
Hinken <henrkri@gmail.com>
2020-04-07 21:26:54 +0100

    done a lot of work on the newer site

--- drafts/script.js
+++ drafts/script.js
@@ -11,7 +11,8 @@
 	});
 });*/
 
-var anchors = $('.section');
+/*
+var anchors = $('section');
 
 $(document).on("scroll", onScroll);
 	function onScroll() {
@@ -25,6 +26,96 @@ $(document).on("scroll", onScroll);
 			}
 		});
 };
+*/
+
+/*
+
+$(document).on("scroll", function () {
+
+	var pageTop = $(document).scrollTop();
+	var pageBottom = pageTop + $(window).height();
+
+	var allTags = $("section");
+
+	for (var i = 0; i < allTags.length; i++) {
+
+		var tag = allTags[i];
+
+		if (($(tag).position().top) < (pageBottom - $(window).height()/1.3)){
+			$(tag).addClass("visible");
+		}else{
+			$(tag).removeClass("visible");
+		}
+
+		if( (pageBottom*0.8) > (($(allTags[0]).position().top) + $(window).height())  ){
+			//console.log("hello");
+			$(tag).removeClass("visible");
+		}else{
+			$(tag).addClass("visible");
+		}
+	}
+	console.log("---");
+	console.log(($(allTags[0]).position().top) + $(window).height());
+	console.log(pageBottom*0.8);
+	console.log("---");
+
+	
+	console.log("");
+	console.log("Start");
+	console.log(pageTop);
+	console.log(pageBottom);
+	console.log("Diff: "+ (pageBottom-pageTop));
+	console.log("--");
+	console.log($(tags[1]).attr("id")+": "+$(tags[1]).position().top);
+	
+
+	//bottom of section (tag.top + window.height) <--
+	//if bottom of section is == window.hegith*0.5, remove visibility
+});
+*/
+
+/*http://jsfiddle.net/eLwex993/5/*/
+
+$(window).on("load",function() {
+
+	function fade(pageLoad) {
+		var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
+	  	var min=0, max=1, threshold=0.01;
+	  
+	  	$("section").each(function() {
+		  
+			/* Check the location of each desired element */
+			var objectHeight=$(this).outerHeight(), objectTop=($(this).offset().top), objectBottom=$(this).offset().top+(objectHeight);
+			
+			/* Fade element in/out based on its visible percentage */
+			if (objectTop < windowTop) {
+				if (objectBottom > windowTop) {
+					$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/(objectHeight))));
+				}else if ($(this).css("opacity")>=min+threshold || pageLoad) {
+					$(this).fadeTo(0,min);
+				}
+			} else if (objectBottom > windowBottom) {
+				if (objectTop < windowBottom) {
+					$(this).fadeTo(0,min+((max-min)*((windowBottom-objectTop)/objectHeight)));
+				}else if ($(this).css("opacity")>=min+threshold || pageLoad) {
+					$(this).fadeTo(0,min);
+				}
+			} else if ($(this).css("opacity")<=max-threshold || pageLoad) {
+				$(this).fadeTo(0,max);
+			}
+
+		});
+
+	} fade(true); //fade elements on page-load
+	$(window).scroll(function(){
+
+		fade(false);
+		getCurrentSection();
+	
+	}); //fade elements on scroll
+	
+});
+
 
 /*
 $(document).ready(function(){
@@ -38,10 +129,46 @@ $(document).ready(function(){
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
 		e.preventDefault();
-		console.log(e);
+		//console.log(e);
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
 
+
+/*
+(function($) {
+  
+	$.fn.visible = function(partial) {
+		
+		var $t            = $(this),
+			$w            = $(window),
+			viewTop       = $w.scrollTop(),
+			viewBottom    = viewTop + $w.height(),
+			_top          = $t.offset().top,
+			_bottom       = _top + $t.height(),
+			compareTop    = partial === true ? _bottom : _top,
+			compareBottom = partial === true ? _top : _bottom;
+		
+		return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+	};
+	  
+	})(jQuery);
+	
+	$(window).scroll(function(event) {
+		
+		$("section").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+			el.addClass("visible"); 
+			var id = el.attr("id");
+			console.log(id);
+		} else {
+			el.removeClass("visible");
+		}
+	});
+	
+});
+  */
