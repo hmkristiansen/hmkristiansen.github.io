ac6ec85c80efd820832c6408dbfb6fd55eeaea71
Hinken <henrkri@gmail.com>
2020-04-12 12:03:25 +0100

    made the mobile nav much better

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 var browser; // 1 = Chromium, 2 = Webkit
 var isMobile = false;
 
+
+
 /* Page startup and variable events */
 
 $(document).ready(function() {
@@ -19,10 +21,12 @@ $(document).ready(function() {
 	});
 
 	if(isMobile){
+		mobileNavUpdate();
 		removeNav();
 	}
 
 });
+
 
 $('html, body').scroll(function(e) {
 	updateAnchors(e);
@@ -30,6 +34,7 @@ $('html, body').scroll(function(e) {
 		useScrollSpeed(e);
 	}
 }).scroll();
+
 
 /* Supporting Functions */
 
@@ -49,12 +54,18 @@ function startup(){
 		}
 	}
 
+	/*
+	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		document.getElementById('goDark').setAttribute("class", "currentTheme");
+	}else{
+		document.getElementById('goLight').setAttribute("class", "currentTheme");
+	}
+	*/
 }
 
 /* - - - */ 
 
 function createAnchorLinks(){
-
 	var mainSections = document.querySelectorAll("section");
 	$('a[href*="#"]').bind('click', function(e) {
 		e.preventDefault(); 
@@ -74,7 +85,6 @@ function createAnchorLinks(){
 		removeNav();
 		return false;
 	});
-
 }
 
 /* - - - */ 
@@ -83,7 +93,7 @@ function updateAnchors(e){
 
 	var mainSections = document.querySelectorAll("section");
 	var scrollPos = $('body').scrollTop();
-	var reach = window.innerHeight/4;
+	var reach = window.innerHeight/2;
 	var currentRange;
 
 	for(var i=0; i<mainSections.length; i++){
@@ -94,7 +104,8 @@ function updateAnchors(e){
 			//console.log(mainSections[i].id + " in range");
 			$("a[href='"+activeLinkHref+"']").addClass("active");
 			if(isMobile){
-				scrollMobileNav();
+				scrollMobileNav(i);
+				debounce(scrollMobileNav);
 			}
 		}else{
 			$("a[href='"+activeLinkHref+"']").removeClass("active");
@@ -103,71 +114,32 @@ function updateAnchors(e){
 	}
 }
 
-function scrollMobileNav(){
+var mobileNavElements = [];
+var distances = [];
+function mobileNavUpdate(){
+	$('#menu a').each(function(i, li) {
+		var $navEl = $(li);  
+		mobileNavElements[i] = $navEl;
 
-	var scrollPos = $('.active').offset().left;// + $('.active').outerWidth(true)/2;
-	$('#menu').scrollLeft(scrollPos);
-	console.log(scrollPos);
+		var distance = mobileNavElements[i].offset().left;
+		distances[i] = distance;
+	});	
+}
 
-	/*
-	var offset = $('.active').offset();
-	console.log(offset.left);
-	$('nav').scrollLeft(offset.left);
-*/
-
-
-	//var left = position.left;
-	//console.log(position);
-	//console.log(left);
-	//$('nav').scrollBy(position);
-	//var currentChildRight = ('.active').getBoundingClientRect().left;
-
-	//var distance = $('nav').offset - $('.active').offset;
-	//console.log(currentChildRight);
-
-
-	/*
-	var offset = $(".active").offset();
-	var w = $('.nav');
-	alert("(x,y): ("+(offset.left-w.scrollLeft())+","+(offset.top-w.scrollTop())+")");
-*/
-	/*
-	var scrollOffset = $('.nav').getOffset('.active').left;
-	
-	$('.nav').animate({
-		scrollLeft: scrollOffset
-	}, 500);
-
-	*/
-	/*
-	//var mobileNav = $('nav');
-	var liElements = $('ul#menu li');
-
-	for(var i = 0; i < liElements.length; i++){
-		if($($('ul#menu li a')).hasClass('active')){
-			//console.log(true);	
-		}
-	}
-	*/
-
-
-
-	//$('ul#menu li').length
-
-	
+function scrollMobileNav(i){
+	var $width = $(window).width()
+	$('nav').scrollLeft(distances[i] - ($width*0.05));
 }
 
 /* - - - */ 
 
 function updateContainer() {
-
 	var $width = $(window).width();
     if ($width <= 800) {
 		isMobile = true;
     }else {
         isMobile = false;
 	}
-
 }
 
 var resizeId;
