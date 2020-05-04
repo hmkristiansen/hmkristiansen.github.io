6aed113667db9ea140f289b950e5ee697f758569
Hinken <henrkri@gmail.com>
2020-05-04 19:45:04 +0100

    updated testing

--- testing/script/script.js
+++ testing/script/script.js
@@ -1,31 +1,41 @@
+
+function fadeInPage(){
+    $('body').removeClass('loadHide');
+    checkIfTouch();
+}
+
+
+function checkIfTouch(){
+	var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+	if (!touchsupport){ // browser doesn't support touch
+		$('body').addClass('non-touch');
+	}
+}
+
+
 var showWork = false;
 
 window.addEventListener("scroll", function (event) {
 
-    let scroll = this.scrollY;
-
-    let windowHeight = this.innerHeight;
-
-    let workHeight = document.getElementById("work").clientHeight;
-
-    let workDistanceTop = document.getElementById("work").offsetTop;
-
-    let delta = workDistanceTop - scroll;
-
+    let scroll = this.scrollY,
+        windowHeight = this.innerHeight;
+        workHeight = document.getElementById("work").clientHeight;
+        workDistanceTop = document.getElementById("work").offsetTop;
+        delta = workDistanceTop - scroll;
 
     if(delta > 0){
-        if( (delta < (windowHeight/2)) && !showWork){
+        if( (delta < (windowHeight/2.5)) && !showWork){
             showPort(delta);
-        }else if( ( delta > (windowHeight/2)) && showWork){
+        }else if( ( delta > (windowHeight/2.5)) && showWork){
             hidePort(delta);
         }
     }else if(delta < 0){
         let workDistanceBottom = workDistanceTop + workHeight;
         let newDelta = workDistanceBottom - scroll;
 
-        if( (newDelta > (windowHeight/2)) && !showWork){
+        if( (newDelta > (windowHeight/2.5)) && !showWork){
             showPort(delta);
-        }else if( ( newDelta < (windowHeight/2)) && showWork){
+        }else if( ( newDelta < (windowHeight/2.5)) && showWork){
             hidePort(delta);
         }
     }
@@ -36,11 +46,17 @@ function showPort(delta){
     if(delta>0){
         $('#work').removeClass('inactive');
         $('#work').addClass('active');
+
+        $('#greeting').removeClass('active');
+        $('#greeting').addClass('inactive2');
     }else{
         $('#work').removeClass('inactive2');
         $('#work').addClass('active');
+
+        $('#about').removeClass('active');
+        $('#about').addClass('inactive');
+
     }
-    
     showWork = true;
 }
 
@@ -48,9 +64,16 @@ function hidePort(delta){
     if(delta>0){
         $('#work').removeClass('active');
         $('#work').addClass('inactive');
+
+        $('#greeting').addClass('active');
+        $('#greeting').removeClass('inactive2');
     }else{
         $('#work').removeClass('active');
         $('#work').addClass('inactive2');
+
+        $('#about').removeClass('inactive');
+        $('#about').addClass('active');
     }
     showWork = false;
 }
+
