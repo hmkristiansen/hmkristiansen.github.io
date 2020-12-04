607a7cd781f788053e58378edc59f4a4eec258e6
Hinken <henrkri@gmail.com>
2020-12-04 11:45:10 +0100

    layed some foundation for responsveness in js

---
+++ script/responsive.js
@@ -0,0 +1,13 @@
+$(window).resize(function() { 
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    let ratio = w/h;
+    //console.log(ratio);
+
+    if(ratio >= 1){
+        //desktop view
+    }else{
+        //mobile view
+    }
+
+});
