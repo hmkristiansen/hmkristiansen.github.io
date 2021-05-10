836eb789bb967d30e0decbd6c833519248ad1766
Henrik // EGGS <31383225+hmkristiansen@users.noreply.github.com>
2021-05-10 13:11:51 +0200

    fun stuff

--- inf/script.js
+++ inf/script.js
@@ -6,26 +6,32 @@ const body = document.getElementById('body')
 
 let renderAbout = () =>{
     let about = document.createElement('div')
-    about.classList.add("window")
+    about.classList.add("window","draggable")
     about.id = "about"
 
     let title = document.createElement('h1')
+    let cross = document.createElement('img')
+    cross.src = "cross.svg"
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
     title.innerText = "About"
     cont.innerHTML = "Hi, I'm Henrik!<br><br>I create, break, fix, experiment, code and occasionally design stuff. Currently doing an internship at EGGS Design.<br><br>Also, I like running, video games and beagles."
+    
     about.appendChild(title)
+    about.appendChild(cross)
     about.appendChild(cont)
     content.appendChild(about)
 }
 
 let renderWork = () =>{
     let work = document.createElement('div')
-    work.classList.add("window")
+    work.classList.add("window","draggable")
     work.id = "work"
 
     let title = document.createElement('h1')
+    let cross = document.createElement('img')
+    cross.src = "cross.svg"
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
@@ -33,16 +39,19 @@ let renderWork = () =>{
     cont.innerHTML = "I’ve been fortunate enough to work on some cool projects with Cognite, FARA, Anywhere, Santander, Engage NTNU, Novozymes, Tackl, Statens Vegvesen, Lillemini, Trondheim Climathon - just to mention a few.<br><br><a href='https://portfolio.hmkristiansen.no' target='_blank'>Check out my public portfolio</a>"
 
     work.appendChild(title)
+    work.appendChild(cross)
     work.appendChild(cont)
     content.appendChild(work)
 }
 
 let renderContact = () =>{
     let contact = document.createElement('div')
-    contact.classList.add("window")
+    contact.classList.add("window","draggable")
     contact.id = "contact"
 
     let title = document.createElement('h1')
+    let cross = document.createElement('img')
+    cross.src = "cross.svg"
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
@@ -50,6 +59,7 @@ let renderContact = () =>{
     cont.innerHTML = "Currently: CPH<br>Previosly: OSL, LYR, TRD, LIS<br><br><ul><li>Instagram: <a>@hmkristiansen</a></li><li>Twitter: <a>@_hmkristiansen</a></li><li>LinkedIn: <a>@hmkristiansen</a></li><li>Epost: <a>heisann@hmkristiansen.no</a></li></ul><br>Don't be a stranger!"
 
     contact.appendChild(title)
+    contact.appendChild(cross)
     contact.appendChild(cont)
     content.appendChild(contact)
 }
@@ -58,14 +68,30 @@ let renderContent = () =>{
     renderContact()
     renderWork()
     renderAbout()
+    
     body.appendChild(content)
 }
 
 renderContent()
 
+$(function(){
+    $(".draggable").draggableTouch()
+})
+
+$(".window img").click(function(){
+  console.log("Hello")
+  $(this).closest('.window').remove()
+});
+
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  console.log(scroll)
+});
 
 //Make the DIV element draggagle:
 
+/*
 dragElement(document.getElementById("about"));
 dragElement(document.getElementById("work"));
 dragElement(document.getElementById("contact"));
@@ -104,3 +130,4 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+*/
