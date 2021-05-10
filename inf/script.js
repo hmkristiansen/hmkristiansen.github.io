4326049c5553e77529efc160c0ff9086d04d7487
Henrik // EGGS <31383225+hmkristiansen@users.noreply.github.com>
2021-05-10 21:36:56 +0200

    under da hood

--- inf/script.js
+++ inf/script.js
@@ -1,8 +1,9 @@
 
 let content = document.createElement('div')
 content.classList.add("content")
-
 const body = document.getElementById('body')
+
+// Render content
 
 let renderAbout = () =>{
     let about = document.createElement('div')
@@ -10,17 +11,14 @@ let renderAbout = () =>{
     about.id = "about"
 
     let title = document.createElement('h1')
-    let cross = document.createElement('img')
-    cross.src = "cross.svg"
-    cross.classList.add("closeWindow")
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
     title.innerText = "About"
-    cont.innerHTML = "Hi, I'm Henrik!<br><br>I create, break, fix, experiment, code and occasionally design stuff. Currently doing an internship at EGGS Design.<br><br>Also, I like running, video games and beagles."
+    cont.innerHTML = "Hi, I'm Henrik!<br><br>I create, break, fix, experiment, code and occasionally design stuff. Currently doing an internship at EGGS Design.<br><br>I also like running, video games and beagles."
     
     about.appendChild(title)
-    about.appendChild(cross)
+    addCross(about)
     about.appendChild(cont)
     content.appendChild(about)
 }
@@ -31,9 +29,6 @@ let renderWork = () =>{
     work.id = "work"
 
     let title = document.createElement('h1')
-    let cross = document.createElement('img')
-    cross.src = "cross.svg"
-    cross.classList.add("closeWindow")
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
@@ -41,7 +36,7 @@ let renderWork = () =>{
     cont.innerHTML = "I’ve been fortunate enough to work on some cool projects with Cognite, FARA, Anywhere, Santander, Engage NTNU, Novozymes, Tackl, Statens Vegvesen, Lillemini, Trondheim Climathon - just to mention a few.<br><br><a href='https://portfolio.hmkristiansen.no' target='_blank'>Check out my public portfolio</a>"
 
     work.appendChild(title)
-    work.appendChild(cross)
+    addCross(work)
     work.appendChild(cont)
     content.appendChild(work)
 }
@@ -52,17 +47,14 @@ let renderContact = () =>{
     contact.id = "contact"
 
     let title = document.createElement('h1')
-    let cross = document.createElement('img')
-    cross.src = "cross.svg"
-    cross.classList.add("closeWindow")
     let cont = document.createElement('div')
     cont.classList.add("cont")
 
     title.innerText = "Contact"
-    cont.innerHTML = "Currently: CPH<br>Previosly: OSL, LYR, TRD, LIS<br><br><ul><li>Instagram: <a href='https://instagram.com/hmkristiansen' target='_blank'>@hmkristiansen</a></li><li>Twitter: <a>@_hmkristiansen</a></li><li>LinkedIn: <a>@hmkristiansen</a></li><li>Epost: <a>heisann@hmkristiansen.no</a></li></ul><br>Don't be a stranger!"
+    cont.innerHTML = "Currently: CPH<br>Previosly: OSL, LYR, TRD, LIS<br><br><ul><li>Instagram: <a href='https://instagram.com/hmkristiansen' target='_blank'>@hmkristiansen</a></li><li>Twitter: <a>@_hmkristiansen</a></li><li>LinkedIn: <a>hmkristiansen</a></li><li>Epost: <a href='mailto:heisann@hmkristiansen.no'>heisann@hmkristiansen.no</a></li></ul><br>Don't be a stranger!"
 
     contact.appendChild(title)
-    contact.appendChild(cross)
+    addCross(contact)
     contact.appendChild(cont)
     content.appendChild(contact)
 }
@@ -71,66 +63,49 @@ let renderContent = () =>{
     renderContact()
     renderWork()
     renderAbout()
-    
     body.appendChild(content)
 }
-
 renderContent()
+
+// Make content draggable 
 
 $(function(){
     $(".draggable").draggableTouch()
 })
 
-$(".closeWindow").click(function(){
-  console.log("Hello")
+
+// Supporting functions for window-close
+
+$(".closeWindow").click(function closeWindow(){
   $(this).closest('.window').remove()
 });
 
-
-window.addEventListener("scroll", (event) => {
-  let scroll = this.scrollY;
-  console.log(scroll)
-});
-
-//Make the DIV element draggagle:
-
-/*
-dragElement(document.getElementById("about"));
-dragElement(document.getElementById("work"));
-dragElement(document.getElementById("contact"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-  console.log("hello")
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
+function addCross(parent){
+  if (window.ontouchstart === undefined) {
+    let cross = document.createElement('img')
+    cross.src = "cross.svg"
+    cross.classList.add("closeWindow")
+    parent.appendChild(cross)
+  }else{
+    return true
   }
 }
-*/
+
+
+// Add more content on scroll
+
+let h = window.innerHeight
+let counter = 1;
+// console.log(h)
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY
+
+  if(scroll > (h/2)*counter){
+    let cln = content.cloneNode(true)
+    body.appendChild(cln)
+  }
+
+  console.log(scroll)
+})
+
