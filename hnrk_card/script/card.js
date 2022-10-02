e212d130ad0c51a4bd5198870db0fd621b5977da
Henrik <henrkri@gmail.com>
2022-10-02 21:03:13 +0200

    gesgrs

--- hnrk_card/script/card.js
+++ hnrk_card/script/card.js
@@ -4,33 +4,95 @@ class Card {
 
         this.index = index
 
-
+        // defining card
         let card = document.createElement('div')
-
         let cardHeader = document.createElement('div')
-        cardHeader.className = "popup-header noselect"
+        cardHeader.className = "popup-header"
 
-        let cardContent = document.createElement('div')
-        cardContent.className = "cardContent"
 
-        let title = document.createElement('h1')
-        title.innerText = "Henrik Kristiansen"
+        // defining card faces
+        let front = document.createElement('div')
+        front.className = "cardFront"
+        let back = document.createElement('div')
+        back.className = "cardBack"
 
-        let subtitle = document.createElement('h2')
-        subtitle.innerText = "interaction designer"
 
-        let body = document.createElement('p')
-        body.innerText = "I create, break, fix, build, develop, craft, and sometimes design digital experiences."
+        // setting front content
+        let frontTitle = document.createElement('h1')
+        let frontSubtitle = document.createElement('h2')
+        let frontLinks = document.createElement('ul')
 
-        cardContent.appendChild(title)
-        cardContent.appendChild(subtitle)
-        cardContent.appendChild(body)
+        frontTitle.innerText = "Henrik Kristiansen"
+        frontSubtitle.innerHTML = "interface designer<br>osl, nor"
 
-        card.appendChild(cardHeader)
-        card.appendChild(cardContent)
+        let emailLink = document.createElement('a')
+        emailLink.innerText = "Email"
+        emailLink.href = "mailto:heisann@hmkristiansen.no"
+        emailLink.target = "_blank"
+        
+        let igLink = document.createElement('a')
+        igLink.innerText = "Instagram"
+        igLink.href = "instagram.com/hmkristiansen"
+        igLink.target = "_blank"
+
+        let liLink = document.createElement('a')
+        liLink.innerText = "LinkedIn"
+        liLink.href = "linkedin.com/hmkristiansen"
+        liLink.target = "_blank"
+
+        let twLink = document.createElement('a')
+        twLink.innerText = "Twitter"
+        twLink.href = "twitter.com/hmkristiansen"
+        twLink.target = "_blank"
+
+        let links1 = [emailLink, igLink, liLink, twLink]
+        for(let i=0; i<links1.length; i++){
+            let liElement  =document.createElement('li')
+            liElement.appendChild(links1[i])
+            frontLinks.appendChild(liElement)
+        }
+
+        front.appendChild(frontTitle)
+        front.appendChild(frontSubtitle)
+        front.appendChild(frontLinks)
+        front.appendChild(cardHeader)
+
+        // setting back content
+        let backSubtitle = document.createElement('h2')
+        let backLinks = document.createElement('ul')
+
+        backSubtitle.innerHTML = "Co-founder of Squads.<br>Creating future products at ITERATE."
+
+        let blogLink = document.createElement('a')
+        emailLink.innerText = "Blog"
+        emailLink.href = "*"
+        emailLink.target = "_blank"
+        
+        let proLink = document.createElement('a')
+        igLink.innerText = "Projects"
+        igLink.href = "*"
+        igLink.target = "_blank"
+
+        let phoLink = document.createElement('a')
+        liLink.innerText = "Photos"
+        liLink.href = "*"
+        liLink.target = "_blank"
+
+        let links2 = [blogLink, proLink, phoLink]
+        for(let i=0; i<links2.length; i++){
+            let liElement  =document.createElement('li')
+            liElement.appendChild(links2[i])
+            frontLinks.appendChild(liElement)
+        }
+
+        back.appendChild(backSubtitle)
+        back.appendChild(backLinks)
+        back.appendChild(cardHeader)
+
+
+        card.appendChild(front)
 
         card.className = "popup"
-
 
         this.card = card
     }
